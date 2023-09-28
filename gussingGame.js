1b3d2cf8904f62a708776b9c9ff0375b828fb4bb
Mohammad Rubel Mollik <rubel@pop-os.localdomain>
2023-09-28 14:37:07 +0600

    show the result number of won and lost

--- gussingGame.js
+++ gussingGame.js
@@ -4,13 +4,22 @@
 // run the game for five times
 // show the the result number of wons and lost
 
+var  numberOfWon = 0;
+var numberOfLost = 0;
+
+
 for (var i = 1; i <= 5; i++) {
   const gussingNumber = parseInt(prompt("Enter a number from 1 to 5 : "));
   const randomNumber = Math.floor(Math.random() * 5) + 1;
 
   if (gussingNumber === randomNumber) {
-    console.log("You have won");
+      console.log("You have won");
+      numberOfWon++;
   } else {
-    console.log("You have lost. Random number was " + randomNumber);
+      console.log("You have lost. Random number was " + randomNumber);
+      numberOfLost++;
   }
 };
+
+document.write("Total Number of won =" + numberOfWon + "<br>");
+document.write("Total Number of won =" + numberOfLost + "");
